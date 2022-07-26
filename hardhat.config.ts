c6d16bb7facc8ac0c044e1858140fc57ff446345
Starlord0alpha <notgoatmichael@gmail.com>
2022-07-26 19:11:09 +0800

    finish

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,88 @@
-import { HardhatUserConfig } from "hardhat/config";
-import "@nomicfoundation/hardhat-toolbox";
+import * as dotenv from "dotenv";
+
+import { HardhatUserConfig, task } from "hardhat/config";
+import "@nomiclabs/hardhat-etherscan";
+import "@nomiclabs/hardhat-waffle";
+import "@typechain/hardhat";
+import "@openzeppelin/hardhat-upgrades";
+import "hardhat-gas-reporter";
+import "solidity-coverage";
+
+dotenv.config();
+
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.9",
+  solidity: {
+    version: "0.8.4",
+    settings: {
+      optimizer: { enabled: true, runs: 100 },
+      evmVersion: "berlin",
+      // for smock to mock contracts
+      outputSelection: {
+        "*": {
+          "*": ["storageLayout"],
+        },
+      },
+    },
+  },
+  networks: {
+    ropsten: {
+      url: process.env.ROPSTEN_URL || "",
+      accounts:
+          process.env.DEVNET_PRIVKEY !== undefined
+              ? [process.env.DEVNET_PRIVKEY]
+              : [],
+    },
+    rinkeby: {
+      url: process.env.RINKEBY_RPC || "",
+      accounts:
+          process.env.DEVNET_PRIVKEY !== undefined
+              ? [process.env.DEVNET_PRIVKEY]
+              : [],
+    },
+    arbitrumOne: {
+      url: process.env.ARBITRUM_ONE_RPC || "",
+      accounts:
+          process.env.DEVNET_PRIVKEY !== undefined
+              ? [process.env.DEVNET_PRIVKEY]
+              : [],
+    },
+    arbitrumTestnet: {
+      url: process.env.ARBITRUM_TESTNET_RPC || "",
+      accounts:
+          process.env.DEVNET_PRIVKEY !== undefined
+              ? [process.env.DEVNET_PRIVKEY]
+              : [],
+    },
+    matic: {
+      url: process.env.MATIC_PRC || "",
+      accounts:
+          process.env.DEVNET_PRIVKEY !== undefined
+              ? [process.env.DEVNET_PRIVKEY]
+              : [],
+    },
+    mumbai: {
+      url: process.env.MUMBAI_PRC || "",
+      accounts:
+          process.env.DEVNET_PRIVKEY !== undefined
+              ? [process.env.DEVNET_PRIVKEY]
+              : [],
+    },
+  },
+  gasReporter: {
+    enabled: process.env.REPORT_GAS !== undefined,
+    currency: "USD",
+  },
+  etherscan: {
+    apiKey: process.env.POLYSCAN_API_KEY,
+  },
 };
 
 export default config;
--- scripts/upgrade.ts
+++ scripts/upgrade.ts
@@ -1,10 +1,12 @@
 import {ethers, upgrades} from "hardhat";
 
 async function main() {
+    const proxyAddress = '0x8b7b9EB688c0aa00D3c53Eb369c88E4386750966';
+
     const Demo = await ethers.getContractFactory("Demo");
-    console.log("Deploying Demo...");
-    const demo = await upgrades.deployProxy(Demo, [101], {initializer: 'initialize'});
-    console.log("Demo deployed to:", demo.address);
+    console.log("Preparing upgrade...");
+    // 升级合约
+    await upgrades.upgradeProxy(proxyAddress, Demo);
 }
 
 main().catch((error) => {
